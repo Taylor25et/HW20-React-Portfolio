f943ecc346589fc5d0a8890971ae09b677e9cc7b
Taylor25et <taylor25et@gmail.com>
2022-06-05 19:25:21 -0700

    fixing card linking issues

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -27,7 +27,7 @@ class Portfolio extends Component {
                     cardImage='https://github.com/Taylor25et/Final-Project-Dating-Sim/raw/main/client/public/assets/images/SignUpScreenshot.png'
                     githubLink='https://github.com/Taylor25et/Final-Project-Dating-Sim'
                     hostedLink='https://calm-hamlet-19238.herokuapp.com/'
-                    isHosted={false}
+                    isHosted={true}
                 />
 
                 <Project
@@ -38,7 +38,7 @@ class Portfolio extends Component {
                     cardImage='https://github.com/Taylor25et/HW14-Tech-Blog/raw/main/public/assets/images/home.png'
                     githubLink='https://github.com/Taylor25et/HW14-Tech-Blog'
                     hostedLink='https://radiant-basin-40892.herokuapp.com/'
-                    isHosted={false}
+                    isHosted={true}
                 />
 
                 <Project
@@ -61,126 +61,3 @@ class Portfolio extends Component {
 }
 export default Portfolio;
 
-// EVERYTHING ABOVE HERE IS NEW
-
-// import * as React from 'react';
-// import ImageList from '@mui/material/ImageList';
-// import ImageListItem from '@mui/material/ImageListItem';
-// import ImageListItemBar from '@mui/material/ImageListItemBar';
-// import ListSubheader from '@mui/material/ListSubheader';
-// import IconButton from '@mui/material/IconButton';
-// import InfoIcon from '@mui/icons-material/Info';
-// // import Project from './Project';
-// // import Footer from "./Footer";
-
-
-
-
-// export default function Portfolio(props) {
-//   return (
-//     // <Project />
-//     <ImageList sx={{ width: 500, height: 450 }}>
-//       <ImageListItem key="Subheader" cols={2}>
-//         <ListSubheader component="div">Portfolio</ListSubheader>
-//       </ImageListItem>
-//       {itemData.map((item) => (
-//         <ImageListItem key={item.img}>
-//           <img
-//             src={`${item.img}?w=248&fit=crop&auto=format`}
-//             srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-//             alt={item.title}
-//             loading="lazy"
-//           />
-//           <ImageListItemBar
-//             title={item.title}
-//             subtitle={item.author}
-//             actionIcon={
-//               <IconButton
-//                 sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-//                 aria-label={`info about ${item.title}`}
-//               >
-//                 <InfoIcon />
-//               </IconButton>
-//             }
-//           />
-//         </ImageListItem>
-//       ))}
-//     </ImageList>
-//   );
-// }
-
-// const itemData = [
-//   {
-//     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-//     title: 'Code Quiz',
-//     author: '@bkristastucchio',
-//     rows: 2,
-//     cols: 2,
-//     featured: true,
-//   },
-//   {
-//     img: 'https://github.com/Taylor25et/Project-2-Travel-App/raw/main/assets/home-page.png',
-//     title: 'Travel Application',
-//     author: 'https://travalotpj2.herokuapp.com/',
-//     //https://github.com/Taylor25et/Project-2-Travel-App
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-//     title: 'Day Planner',
-//     author: 'https://taylor25et.github.io/HW5-Day-Planner/',
-//     //https://github.com/Taylor25et/HW5-Day-Planner
-//   },
-//   {
-//     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-//     title: 'Travel App',
-//     author: '@nolanissac',
-//     cols: 2,
-//   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-// //     title: 'Hats',
-// //     author: '@hjrc33',
-// //     cols: 2,
-// //   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-// //     title: 'Honey',
-// //     author: '@arwinneil',
-// //     rows: 2,
-// //     cols: 2,
-// //     featured: true,
-// //   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-// //     title: 'Basketball',
-// //     author: '@tjdragotta',
-// //   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-// //     title: 'Fern',
-// //     author: '@katie_wasserman',
-// //   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-// //     title: 'Mushrooms',
-// //     author: '@silverdalex',
-// //     rows: 2,
-// //     cols: 2,
-// //   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-// //     title: 'Tomato basil',
-// //     author: '@shelleypauls',
-// //   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-// //     title: 'Sea star',
-// //     author: '@peterlaster',
-// //   },
-// //   {
-// //     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-// //     title: 'Bike',
-// //     author: '@southside_customs',
-// //     cols: 2,
-// //   },
-// ]; 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,131 +1,219 @@
 import React, { Component } from "react";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+//styling imports
+import CloseIcon from "@mui/icons-material/Close";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Avatar,
+  Grow,
+  Button,
+  CardHeader,
+  Card,
+  Dialog,
+  Divider,
+  CardMedia,
+  CardActions,
+  CardContent,
+  IconButton,
+  DialogTitle,
+  DialogContent,
+  Grid,
+  Typography,
+  ListItemAvatar,
+  ListItemText,
+  ListItem,
+} from "@mui/material";
+
+const styles = (theme) => ({
+  listItem: {
+    display: "none",
+    [theme.breakpoints.down("xs")]: {
+      display: "flex",
+      width: "100vw",
+    },
+  },
+  cardImage: {
+    width: "100%",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  dialogTitle: {
+    display: "flex",
+    alignItems: "center",
+  },
+  dialogContent: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+  },
+  closeIcon: {
+    width: 32,
+    height: 32,
+  },
+  avatar: {
+    width: 72,
+    height: 72,
+  },
+  card: {
+    maxWidth: 450,
+    padding: 10,
+    margin: 16,
+    height: "100%",
+  },
+  gridItem: {
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
+  },
+});
+
 class Project extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ExpandMore: false,
+      isDialogOpen: false,
     };
   }
 
-  // toggleDialog(){
-  //   const {isDialogOpen} = this.state;
-  //   this.setState({isDialogOpen:!isDialogOpen})
-  // }
-  render() {
-    const { title } = this.props;
-    const [expanded, setExpanded] = this.state;
+  toggleDialog() {
+    const { isDialogOpen } = this.state;
+    this.setState({ isDialogOpen: !isDialogOpen });
+  }
 
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
-    // const {isDialogOpen} = this.state;
+  render() {
+    const {
+      title,
+      iconSrc,
+      cardText,
+      cardImage,
+      subtitle,
+      classes,
+      isHosted,
+      githubLink,
+      hostedLink,
+    } = this.props;
+    const { isDialogOpen } = this.state;
     return (
       <div>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                R
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title={title}
-            subheader="September 14, 2016"
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image="/static/images/cards/paella.jpg"
-            alt="Paella dish"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {/* {cardText} */}
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>
-            <ExpandMore
-              expand={expanded}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
+        <ListItem
+          className={classes.listItem}
+          button
+          divider
+          onClick={() => this.toggleDialog()}
+        >
+          <ListItemAvatar>
+            <Avatar className={classes.avatar} src={iconSrc}>
+              {title.charAt(0)}
+            </Avatar>
+          </ListItemAvatar>
+
+          <ListItemText
+            primaryTypographyProps={{ component: "h5", variant: "h5" }}
+            secondary={subtitle}
+          >
+            {title}
+          </ListItemText>
+
+          <Dialog open={isDialogOpen} TransitionComponent={Grow} fullScreen>
+            <DialogTitle
+              className={classes.dialogTitle}
+              disableTypography={true}
             >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography paragraph>Method:</Typography>
-              <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron
-                and set aside for 10 minutes.
+              <IconButton>
+                <CloseIcon className={classes.closeIcon} />
+              </IconButton>
+              <Typography component="h5" variant="h5">
+                Project Details
               </Typography>
-              <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                skillet over medium-high heat. Add chicken, shrimp and chorizo,
-                and cook, stirring occasionally until lightly browned, 6 to 8
-                minutes. Transfer shrimp to a large plate and set aside, leaving
-                chicken and chorizo in the pan. Add pimentón, bay leaves,
-                garlic, tomatoes, onion, salt and pepper, and cook, stirring
-                often until thickened and fragrant, about 10 minutes. Add
-                saffron broth and remaining 4 1/2 cups chicken broth; bring to a
-                boil.
-              </Typography>
-              <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes
-                and peppers, and cook without stirring, until most of the liquid
-                is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
-                reserved shrimp and mussels, tucking them down into the rice,
-                and cook again without stirring, until mussels have opened and
-                rice is just tender, 5 to 7 minutes more. (Discard any mussels
-                that don&apos;t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then
-                serve.
-              </Typography>
-            </CardContent>
-          </Collapse>
-        </Card>
+            </DialogTitle>
+
+            <Divider />
+
+            <DialogContent className={classes.dialogContent}>
+              <div>
+                <img className={classes.cardImage} src={cardImage} />
+                <Typography component="h4" variant="h4">
+                  {title}
+                </Typography>
+                <Typography variant="subtitle2">{cardText}</Typography>
+              </div>
+              <div>
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  component="a"
+                  href={githubLink}
+                  target="blank"
+                >
+                  View GitHub Repository
+                </Button>
+                <Button
+                  fullWidth
+                  variant="outlined"
+                  component="a"
+                  href={hostedLink}
+                  target="blank"
+                  disabled={!isHosted}
+                >
+                  View Hosted Example
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
+        </ListItem>
+
+        <Grid
+          className={classes.gridItem}
+          item
+          xs={12}
+          style={{ height: "95%" }}
+        >
+          <Card className={classes.card}>
+            <CardHeader
+              avatar={<Avatar src={iconSrc}></Avatar>}
+              title={title}
+              subheader={subtitle}
+              titleTypographyProps={{
+                component: "h4",
+                variant: "h4",
+                align: "left",
+              }}
+            />
+
+            <CardMedia
+              className={classes.cardImage}
+              image={cardImage}
+              component="img"
+            />
+
+            <CardContent>{cardText}</CardContent>
+
+            <CardActions>
+              <Button
+                fullWidth
+                variant="outlined"
+                component="a"
+                href={hostedLink}
+                target="blank"
+                disabled={!isHosted}
+              >
+                View Deployed Site
+              </Button>
+              <Button
+                fullWidth
+                variant="outlined"
+                component="a"
+                href={githubLink}
+                target="blank"
+              >
+                View GitHub Repository
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </div>
     );
   }
 }
 
-export default Project;
+export default withStyles(styles)(Project);
