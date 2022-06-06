2247d0d3f21d999e23325a5f0688e93ea4e075dd
Taylor25et <taylor25et@gmail.com>
2022-06-05 17:02:57 -0700

    adding card classes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,42 +1,65 @@
-import * as React from 'react';
-// import { styled } from '@mui/material/styles';
-// import Card from '@mui/material/Card';
-// import CardHeader from '@mui/material/CardHeader';
-// import CardMedia from '@mui/material/CardMedia';
-// import CardContent from '@mui/material/CardContent';
-// import CardActions from '@mui/material/CardActions';
-// import Collapse from '@mui/material/Collapse';
-// import Avatar from '@mui/material/Avatar';
-// import IconButton from '@mui/material/IconButton';
-// import Typography from '@mui/material/Typography';
-// import { red } from '@mui/material/colors';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
-// import ShareIcon from '@mui/icons-material/Share';
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
+import React,{Component} from 'react';
+
 import Footer from './Footer';
 import Project from './Project';
 
+import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-const theme = createTheme();
 
-export default function Portfolio(props) {
-  // const [expanded, setExpanded] = React.useState(false);
-
-  // const handleExpandClick = () => {
-  //   setExpanded(!expanded);
-  // };
-
+class Portfolio extends Component {
+  constructor(props){
+    super(props);
+    
+  }
+  render(){
+   const theme = createTheme();
   return (
     <ThemeProvider theme={theme}>
-      <Project />
+      <List>
+            <Grid container justify='center'>
+                <Project
+                    iconSrc='https://avatars.githubusercontent.com/u/95151361?v=4'
+                    title='Dating Simulator'
+                    subtitle='UW Final Group Project'
+                    cardText='Data-A-Base is a dating simulator experience that will pull your heartstrings and build new connections as users start their journey to becoming a full-stack web developer.'
+                    cardImage='https://github.com/Taylor25et/Final-Project-Dating-Sim/raw/main/client/public/assets/images/SignUpScreenshot.png'
+                    githubLink='https://github.com/Taylor25et/Final-Project-Dating-Sim'
+                    hostedLink='https://calm-hamlet-19238.herokuapp.com/'
+                    isHosted={false}
+                />
+
+                <Project
+                    iconSrc='https://avatars.githubusercontent.com/u/95151361?v=4'
+                    title='Tech Blog'
+                    subtitle="Coding Blog Project"
+                    cardText='This application is a CMS-style blog site where developers can publish their blog posts and comment on other developers’ posts. This app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.'
+                    cardImage='https://github.com/Taylor25et/HW14-Tech-Blog/raw/main/public/assets/images/home.png'
+                    githubLink='https://github.com/Taylor25et/HW14-Tech-Blog'
+                    hostedLink='https://radiant-basin-40892.herokuapp.com/'
+                    isHosted={false}
+                />
+
+                <Project
+                    iconSrc='https://avatars.githubusercontent.com/u/95151361?v=4'
+                    title='Taveling Site'
+                    subtitle='Browse destinations by their attaractions'
+                    cardText='This travel application is designed to present users with four different options for travel destinations like theme parks, water parks, resorts, and specialty rollercoasters. Each subsequent page displays the top five destinations/parks for each category that showcases information like the website, address, yearly visitors, and a brief description of the location. '
+                    cardImage='https://github.com/Taylor25et/Project-2-Travel-App/raw/main/assets/home-page.png'
+                    githubLink='https://github.com/Taylor25et/Project-2-Travel-App'
+                    hostedLink='https://travalotpj2.herokuapp.com/'
+                    isHosted={true}
+                />
+            </Grid>
+            </List>
       <Footer sx={{ mt: 5 }} />
 
     </ThemeProvider>
   );
+ }
 }
-
+export default Portfolio;
 
 // EVERYTHING ABOVE HERE IS NEW
 
